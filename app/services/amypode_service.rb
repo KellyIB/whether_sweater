e56732cba8e32d03ff3dc3f7d1488faa46ba9dc2
KellyIB <kellyibard@gmail.com>
2020-04-20 10:11:01 -0600

    work on final - pom and commit break

--- app/services/amypode_service.rb
+++ app/services/amypode_service.rb
@@ -2,20 +2,28 @@ class AmypodeService < GoogleService
 
   def get_antipode(start_city)
     data = get_geocode_data(start_city)
-    binding.pry
     lat = data[:results][0][:geometry][:location][:lat].round(0)
     long = data[:results][0][:geometry][:location][:lng].round(0)
 
-
-    connection = Faraday.new(url: "http://amypode.herokuapp.com") do |faraday|
-      faraday.headers["api_key"] = ENV['Amypode_APIKEY']
-    end
-
-    response = connection.get("/maps/api/geocode/json") do |f|
+    resp = conn.get("/api/v1/antipodes") do |f|
       f.params["lat"] = lat
       f.params["long"] = long
     end
-
-    JSON.parse(response.body, symbolize_names: true)
+    get_antipode_json(resp)
   end
+
+
+  private
+
+  def get_antipode_json(response)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+  end
+
+  def conn
+    conn = Faraday.new(url: "http://amypode.herokuapp.com") do |faraday|
+      faraday.headers["api_key"] = ENV['Amypode_APIKEY']
+    end
+  end
+
 end
