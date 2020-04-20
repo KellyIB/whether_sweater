9ccc68a63788dfbc3e48baf55f0da21082ef19e2
KellyIB <kellyibard@gmail.com>
2020-04-20 11:58:19 -0600

    final commit - time's up!

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -2,7 +2,7 @@ class Location
   attr_reader :place_id, :lat, :lon, :formatted_address
   def initialize(geocode_data, search_facade)
     @search = search_facade
-    @formatted_address = geocode_data[:results][0][:formatted_address]
+    @formatted_address = geocode_data[:results][2][:formatted_address]
     @lat = geocode_data[:results][0][:geometry][:location][:lat]
     @lon = geocode_data[:results][0][:geometry][:location][:lng]
     @place_id = geocode_data[:results][0][:place_id]
