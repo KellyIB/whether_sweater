6551a22ee1baacbd6ec56f28b42eb54f8b0c31d5
KellyIB <kellyibard@gmail.com>
2020-04-21 01:39:40 -0600

    add roat trips controller

---
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -0,0 +1,11 @@
+# rubocop:disable Style/ClassAndModuleChildren
+
+class Api::V1::RoadTripsController < ApplicationController
+
+  def create
+
+    binding.pry
+  end
+end
+
+# rubocop:enable Style/ClassAndModuleChildren
