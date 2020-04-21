97b20aafe6575371073bc0849e60c8f7ec00f054
KellyIB <kellyibard@gmail.com>
2020-04-21 01:38:56 -0600

    add routes for session, registration, and road_trip

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :users, only: :create
       get '/forecast', to: 'forecasts#show'
-      get 'backgrounds', to: 'backgrounds#show'
+      get '/backgrounds', to: 'backgrounds#show'
+      post '/sessions', to: 'sessions#create'
+      post 'road_trip', to: 'road_trips#create'
     end
   end
 end
